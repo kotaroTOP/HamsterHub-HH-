ce350035c3ac0560bcdba9d29d404de979aa4041
kotaroTOP <135441002+kotaroTOP@users.noreply.github.com>
2025-04-23 19:31:48 +0300

    Create ScriptConnecter.lua

---
+++ ScriptConnecter.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kotaroTOP/dws/refs/heads/main/Script1.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kotaroTOP/dws/refs/heads/main/Script2.lua"))()
