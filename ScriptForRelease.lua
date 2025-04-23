e862c2126c232e37bf03b7d8bfd3745dfc69c532
kotaroTOP <135441002+kotaroTOP@users.noreply.github.com>
2025-04-23 19:33:45 +0300

    Create ScriptForRelease.lua

---
+++ ScriptForRelease.lua
@@ -0,0 +1,2 @@
+--All scripts are not mine its just connection of other DW scripts! All credits belong to autors.
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kotaroTOP/dws/refs/heads/main/ScriptConnector.lua"))()
