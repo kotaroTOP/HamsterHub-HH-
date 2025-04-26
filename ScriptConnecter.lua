e49478f082da350cec5094034be2d3fe1e70ece5
kotaroTOP <135441002+kotaroTOP@users.noreply.github.com>
2025-04-26 18:25:56 +0300

    Update ScriptConnecter.lua

--- ScriptConnecter.lua
+++ ScriptConnecter.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/kotaroTOP/dws/refs/heads/main/Script1.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/kotaroTOP/dws/refs/heads/main/Script2.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kotaroTOP/HamsterHub-HH-/refs/heads/main/Script1.lua?token=GHSAT0AAAAAADCW64MCJER5ZZFANJGRRL4C2AM7MFA"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kotaroTOP/HamsterHub-HH-/refs/heads/main/Script2.lua?token=GHSAT0AAAAAADCW64MDQNMHD2YAPBEJKY5C2AM7MPA"))()
